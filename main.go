12e63a5fd5c55bfa00d0cffa00313f5889fc89d7
y836475809 <tttdaytttday@gmail.com>
2025-08-22 01:12:10 +0900

    add test
    use gnock
    $ go test -v req1_test.go main.go

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Sample struct {
+	ID   int    `json: "id"`
+	Name string `json: "name"`
+}
 
 func main() {
 	router := gin.Default()
@@ -14,4 +27,36 @@ func main() {
 
 	// サーバーを起動
 	router.Run(":8080") // ポート8080でリッスン
+}
+
+func StartGet(c *gin.Context) {
+	url := "http://foo.com/bar?a=1&b=2"
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("err=%v\n", err)
+	}
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("err=%v\n", err)
+	}
+	c.JSON(200, gin.H{"key": string(b)})
+}
+
+func StartPost(c *gin.Context) {
+	sample := new(Sample)
+	sample.ID = 0
+	sample.Name = "hoge"
+	sample_json, _ := json.Marshal(sample)
+	url := "http://foo.com/post?a=1&b=10"
+	res, err := http.Post(url,
+		"application/json",
+		bytes.NewBuffer(sample_json))
+	if err != nil {
+		fmt.Printf("err=%v\n", err)
+	}
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("err=%v\n", err)
+	}
+	c.JSON(200, gin.H{"key": string(b)})
 }
